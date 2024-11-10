aa0c004d67fd5e4f38024d413f9778c1c1b269cb
DAVV422 <diegoalberto42216@gmail.com>
2024-11-10 09:27:02 -0400

    fix(alumno): fix relaciones con curso y tutores

--- extra_addons/agenda/models/alumno.py
+++ extra_addons/agenda/models/alumno.py
@@ -9,7 +9,6 @@ class Alumno(models.Model):
     edad = fields.Integer("Edad", required=True)
     tutor_id = fields.Many2one('colegio.tutor', string="Tutor")
     grado = fields.Many2one('colegio.curso', string="Grado Actual")
-    curso_ids = fields.Many2many('colegio.curso', through='colegio.inscripcion', string="Cursos")
     
     @api.model
     def create(self, vals):
